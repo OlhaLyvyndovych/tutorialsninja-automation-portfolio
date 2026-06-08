0018d82a451c1255ca4d33122b5dcf6a7fdf9aeb
Olha Lyvyndovych <olha.lyvyndovych@gmail.com>
2026-06-08 23:54:49 +0200

    Enable full tracing and lower workflow timeout to debug freeze

--- playwright.config.ts
+++ playwright.config.ts
@@ -30,7 +30,8 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'on',
+    video: 'on',
     viewport: { width: 1280, height: 720 },
   },
 
